107669907524f410512cd50a196149d978d5a2de
unknown <kamal3heydari@gmail.com>
2024-06-30 16:33:09 +0430

    handle delete category

--- src/pages/api/category/[id]/index.js
+++ src/pages/api/category/[id]/index.js
@@ -1,3 +1,39 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" })
+import db from "@/lib/db"
+
+import Word from "@/models/Word"
+import Category from "@/models/Category"
+
+const handler = async (req, res) => {
+  switch (req.method) {
+    // case 'GET':
+    //   await getCategory(req, res)
+    //   break
+
+    // case 'PUT':
+    //   await updateCategory(req, res)
+    //   break
+
+    case "DELETE":
+      await deleteCategory(req, res)
+      break
+
+    default:
+      break
+  }
 }
+
+const deleteCategory = async (req, res) => {
+  try {
+    await db.connect()
+    const { id } = req.query
+    console.log(id)
+    await Category.findByIdAndDelete(id)
+    await Word.deleteMany({ category: id })
+    res.status(200).json({ msg: "category deleted" })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: "Failed to delete category" })
+  }
+}
+
+export default handler
--- src/pages/category/index.jsx
+++ src/pages/category/index.jsx
@@ -1,7 +1,9 @@
+import { useRouter } from "next/router"
 import db from "@/lib/db"
 import Category from "@/models/Category"
 import Head from "next/head"
 import Link from "next/link"
+import { toast } from "react-toastify"
 
 export const getServerSideProps = async () => {
   await db.connect()
@@ -17,7 +19,37 @@ export const getServerSideProps = async () => {
 
 const CategoriesPage = (props) => {
   const { categories } = props
+  const { reload } = useRouter()
 
+  // Handlers
+  const handleDelete = async (id) => {
+    const deleteResult = confirm("Are you sure you want to delete this item?")
+    console.log(deleteResult)
+    if (deleteResult) {
+      try {
+        const response = await fetch("/api/category/" + id, {
+          method: "DELETE",
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        })
+
+        if (!response.ok) {
+          toast.error(`Error fetching data: ${response.status}`)
+          throw new Error(`Error fetching data: ${response.status}`)
+        } else {
+          const data = await response.json()
+          toast.success(data.msg)
+
+          setTimeout(() => {
+            reload()
+          }, 2000)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
   return (
     <>
       <Head>
@@ -27,13 +59,20 @@ const CategoriesPage = (props) => {
         <div className='w-full items-center justify-between font-mono text-sm'>
           <div className='flex flex-col gap-4'>
             {categories.map((item) => (
-              <Link
-                key={item._id}
-                href={"/category/" + item._id}
-                className='border-2 border-blue-700 hover:border-blue-900 text-white p-2 rounded font-bold shadow-md transition duration-300 ease-in-out text-nowrap'
-              >
-                {item.title}
-              </Link>
+              <div key={item._id} className='flex gap-2'>
+                <Link
+                  href={"/category/" + item._id}
+                  className='border-2 border-blue-700 hover:border-blue-900 text-white p-2 rounded font-bold block w-full shadow-md transition duration-300 ease-in-out text-nowrap'
+                >
+                  {item.title}
+                </Link>
+                <button
+                  onClick={() => handleDelete(item._id)}
+                  className='border-2 border-red-700 hover:border-red-900 text-red-700 p-2 rounded font-bold shadow-md transition duration-300 ease-in-out text-nowrap'
+                >
+                  Delete
+                </button>
+              </div>
             ))}
           </div>
         </div>
